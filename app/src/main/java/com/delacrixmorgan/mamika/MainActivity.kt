611c855bbe1d918b81ab5dba1f60722bb0d9bb33
Morgan Koh <morgan@apptivitlab.com>
2018-11-29 11:39:44 +0800

    Add Record Stack with Layouts

--- app/src/main/java/com/delacrixmorgan/mamika/MainActivity.kt
+++ app/src/main/java/com/delacrixmorgan/mamika/MainActivity.kt
@@ -3,6 +3,14 @@ package com.delacrixmorgan.mamika
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * MainActivity
+ * mamika-android
+ *
+ * Created by Delacrix Morgan on 29/11/2018.
+ * Copyright (c) 2018 licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+ */
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/delacrixmorgan/mamika/record/RecordActivity.kt
@@ -0,0 +1,12 @@
+package com.delacrixmorgan.mamika.record
+
+/**
+ * RecordActivity
+ * mamika-android
+ *
+ * Created by Delacrix Morgan on 29/11/2018.
+ * Copyright (c) 2018 licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+ */
+
+class RecordActivity {
+}
