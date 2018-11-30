34ebfe344807c7970c21d2d876ef632b62010dc9
Morgan Koh <morgan@apptivitlab.com>
2018-11-30 15:34:11 +0800

    Import Exoplayer Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.0.2")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
     implementation("com.google.android.material:material:1.1.0-alpha01")
+    implementation("com.google.android.exoplayer:exoplayer:2.9.1")
 }
