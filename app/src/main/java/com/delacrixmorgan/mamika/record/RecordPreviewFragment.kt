0b288b2bb2e57bb9a5faa87f6ac3fbce21479db0
Morgan Koh <morgan@apptivitlab.com>
2018-11-30 15:34:34 +0800

    Able to Preview Captured Video

--- app/src/main/java/com/delacrixmorgan/mamika/record/RecordPreviewFragment.kt
+++ app/src/main/java/com/delacrixmorgan/mamika/record/RecordPreviewFragment.kt
@@ -1,7 +1,25 @@
 package com.delacrixmorgan.mamika.record
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.delacrixmorgan.mamika.R
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelection
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.TransferListener
+import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.fragment_record_preview.*
 
 /**
  * RecordPreviewFragment
@@ -16,8 +34,6 @@ class RecordPreviewFragment : Fragment() {
         const val ARG_RECORD_PREVIEW_VIDEO_URL = "RecordPreview.videoUrl"
         const val ARG_RECORD_PREVIEW_IS_GALLERY_VIDEO = "RecordPreview.isGalleryVideo"
 
-        const val RECORD_LOCATION_ACTIVITY_REQUEST = 1
-
         fun newInstance(videoUrl: String, isGalleryVideo: Boolean): RecordPreviewFragment {
             val fragment = RecordPreviewFragment()
             val args = Bundle()
@@ -28,5 +44,72 @@ class RecordPreviewFragment : Fragment() {
             fragment.arguments = args
             return fragment
         }
+    }
+
+    private lateinit var bandwidthMeter: DefaultBandwidthMeter
+    private lateinit var dataSourceFactory: DefaultDataSourceFactory
+
+    private var trackSelector: DefaultTrackSelector? = null
+    private var simpleExoPlayer: SimpleExoPlayer? = null
+    private var isGalleryVideo: Boolean = false
+    private var videoUrl: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        this.bandwidthMeter = DefaultBandwidthMeter()
+        this.dataSourceFactory = DefaultDataSourceFactory(this.context, Util.getUserAgent(this.context, this.activity?.packageName), this.bandwidthMeter as TransferListener)
+
+        this.arguments?.let {
+            this.videoUrl = it.getString(ARG_RECORD_PREVIEW_VIDEO_URL) ?: ""
+            this.isGalleryVideo = it.getBoolean(ARG_RECORD_PREVIEW_IS_GALLERY_VIDEO)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_record_preview, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initialiseVideoPlayer(this.videoUrl)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    private fun initialiseVideoPlayer(videoUrl: String) {
+        val videoTrackSelectionFactory: TrackSelection.Factory = AdaptiveTrackSelection.Factory(this.bandwidthMeter)
+        val mediaSource = ExtractorMediaSource.Factory(this.dataSourceFactory).createMediaSource(Uri.parse(videoUrl))
+
+        this.trackSelector = DefaultTrackSelector(videoTrackSelectionFactory)
+        this.simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(this.context, this.trackSelector)
+
+        this.simpleExoPlayer?.prepare(mediaSource)
+        this.simpleExoPlayer?.playWhenReady = true
+        this.simpleExoPlayer?.repeatMode = Player.REPEAT_MODE_ALL
+
+        this.playerView.player = this.simpleExoPlayer
+        this.playerView.useController = false
+        this.playerView.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+    }
+
+    private fun releasePlayer() {
+        if (this.simpleExoPlayer == null) {
+            return
+        }
+
+        this.simpleExoPlayer?.stop()
+        this.simpleExoPlayer?.release()
+
+        this.simpleExoPlayer = null
+        this.trackSelector = null
     }
 }
