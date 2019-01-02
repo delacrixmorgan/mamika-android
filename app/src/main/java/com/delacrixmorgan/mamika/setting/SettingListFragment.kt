9bb8d251d1b934f2af1e6037a3172be660610491
Morgan Koh <morgan@apptivitlab.com>
2019-01-02 15:08:05 +0800

    Add Setting Stack

---
+++ app/src/main/java/com/delacrixmorgan/mamika/setting/SettingListFragment.kt
@@ -0,0 +1,12 @@
+package com.delacrixmorgan.mamika.setting
+
+/**
+ * SettingListFragment
+ * mamika-android
+ *
+ * Created by Delacrix Morgan on 02/01/2019.
+ * Copyright (c) 2019 licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+ */
+
+class SettingListFragment {
+}
